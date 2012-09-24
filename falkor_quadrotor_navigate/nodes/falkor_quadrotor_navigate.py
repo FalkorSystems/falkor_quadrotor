87bda844c40002151d756917a518b2ac537bb07f
Sameer Parekh Brenn <sameer@creativedestruction.com>
2012-09-23 20:54:06 -0400

    fix the target pose so that the robot should always be pointed at the beacon

--- falkor_quadrotor_navigate/nodes/falkor_quadrotor_navigate.py
+++ falkor_quadrotor_navigate/nodes/falkor_quadrotor_navigate.py
@@ -9,6 +9,9 @@
 #
 # Then we publish a Pose with the new desired position and orientation of the robot
 # relative to itself
+#
+# The desired orientation should be given the robot's position to the 
+# beacon's position, not the target_point to the beacon
 # 
 # This gives us where the robot needs to move to, relative to itself
 #
@@ -22,15 +25,20 @@ from geometry_msgs.msg import *
 from std_msgs.msg import *
 
 class FalkorQuadrotorNav:
+    def point_to_quaternion( self, point ):
+        bearing = numpy.arctan2( point.y, -point.x )
+        inclination = numpy.arctan2( point.z, numpy.sqrt( numpy.square( point.x ) +
+                                                          numpy.square( point.y ) ) )
+
+        q = Quaternion( *tuple( tf.transformations.quaternion_from_euler( 0.0, inclination, -bearing ) ) )
+        return q
+
     def update_relative_pose( self, data ):
         self.relative_pose = PoseStamped()
         self.relative_pose.pose.position = data.point
         self.relative_pose.header = data.header
 
-        bearing = numpy.arctan2( data.point.y, -data.point.x )
-        inclination = numpy.arctan2( data.point.z, numpy.sqrt( data.point.x*data.point.x +
-                                                               data.point.y*data.point.y ) )
-        self.relative_pose.pose.orientation = Quaternion( *tuple( tf.transformations.quaternion_from_euler( 0.0, inclination, -bearing ) ) )
+        self.relative_pose.pose.orientation = self.point_to_quaternion( data.point )
 
     def __init__( self ):
         self.seq = 0
@@ -39,6 +47,7 @@ class FalkorQuadrotorNav:
         self.subscriber = rospy.Subscriber( 'beacon/target_point', PointStamped,
                                             self.update_relative_pose )
         self.nav_target = rospy.Publisher( 'robot/target_pose', PoseStamped )
+
         relative_point = PointStamped( Header( 0, rospy.Time.now(),
                                               '/beacon/estimate/base_position' ),
                                       Vector3( 1, 0.5, 0.25 ) )
@@ -56,9 +65,9 @@ class FalkorQuadrotorNav:
                                             '/beacon/estimate/base_position', self.relative_pose.header.stamp,
                                             rospy.Duration( 4.0 ) )
 
-            (trans,rot) = self.listener.lookupTransform('/robot/estimate/base_stabilized',
-                                                        '/beacon/estimate/base_position',
-                                                        self.relative_pose.header.stamp )
+            (trans,rot) = self.listener.lookupTransform( '/beacon/estimate/base_position',
+                                                         '/robot/estimate/base_stabilized',
+                                                         self.relative_pose.header.stamp )
         except (tf.LookupException, tf.Exception,
                 tf.ConnectivityException, tf.ExtrapolationException):
             print "waiting Failed"
@@ -68,6 +77,12 @@ class FalkorQuadrotorNav:
         target_pose = self.listener.transformPose( '/robot/estimate/base_stabilized', self.relative_pose )
         target_pose.header.stamp = rospy.Time.now()
         target_pose.header.seq = self.seq
+
+        # target_pose.pose.orientation is the orientation of the 'trans',
+        # which gives me a direction from where the robot is now to the beacon
+        # not from where the robot should be to the beacon
+        point = Point( *trans )
+        target_pose.pose.orientation = self.point_to_quaternion( point )
 
         print target_pose
         self.nav_target.publish( target_pose )
