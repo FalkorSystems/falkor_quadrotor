81230758d4cfbf715000152652ccc2a65d5fd3ff
Sameer Parekh Brenn <sameer@creativedestruction.com>
2012-11-15 14:08:10 -0600

    fix up the pwm driver. have to run as root =(

--- falkor_drivers/nodes/pwm_beagle_driver.py
+++ falkor_drivers/nodes/pwm_beagle_driver.py
@@ -2,7 +2,8 @@
 
 import roslib; roslib.load_manifest("falkor_drivers")
 import rospy
-from falkor_msgs.msg import *
+import struct
+import falkor_msgs.msg
 from mmap import mmap
 
 #
@@ -95,7 +96,7 @@ class Pwm:
         table += fmt % ('CHANNEL', 'HEADER', 'PIN', 'SIGNAL', 'FILE SYSTEM')
 
         for pwm in sorted(pwm_map.iterkeys()):
-            p = pwm_map[pwm]
+            p = Pwm.pwm_map[pwm]
             table += fmt % (pwm, p['header'], p['pin'], p['signal'], p['fs'])
 
         table += "PWM amplitude is 3.3V.\n" 
@@ -107,7 +108,7 @@ class Pwm:
     def setReg(self, address, new_value):
         """ Sets 32 bits at given address to given value. """
         with open("/dev/mem", "r+b") as f:
-            mem = mmap(f.fileno(), MMAP_SIZE, offset=MMAP_OFFSET)
+            mem = mmap(f.fileno(), Pwm.MMAP_SIZE, offset=Pwm.MMAP_OFFSET)
             mem[address:address+4] = struct.pack("<L", new_value)
 
     def writeDevice(self, device, value):
@@ -120,7 +121,8 @@ class Pwm:
         self.writeDevice("%s/duty_percent" % device, dutyPercent)
 
     def setDutyPercentByChannel(self, channel, dutyPercent):
-        dev = pwm_map[channel]['fs']
+        dev = Pwm.pwm_map[channel]['fs']
+	print "setting channel %d to %d" % ( channel, dutyPercent )
         self.setDutyPercent(dev, dutyPercent)
 
     def getDutyPercent(self, device):
@@ -131,7 +133,7 @@ class Pwm:
 
     def setFrequency(self, device, frequencyHz):
         """ Set PWM frequency. """
-        writeDevice("%s/period_freq" % device, frequencyHz)
+        self.writeDevice("%s/period_freq" % device, frequencyHz)
 
     def getFrequency(self, device):
         """ Reads current duty cycle in percent. """
@@ -141,7 +143,7 @@ class Pwm:
 
     def request(self, device, req):
         """ Request or release PWM channel. """
-        writeDevice("%s/request" % device, req)
+        self.writeDevice("%s/request" % device, req)
     
     def run(self, device, start):
         """ Start (1) or Stop (0) PWM. """
@@ -149,7 +151,7 @@ class Pwm:
             s = f.read()
 
         if start != int(s):
-            writeDevice("%s/run" % device, start)
+            self.writeDevice("%s/run" % device, start)
 
     def checkBusy(self, device):
         """ Check if device is busy. """
@@ -162,22 +164,22 @@ class Pwm:
 
     def setMuxMode(self, pinName, mode):
         """ Set mux mode of the pin. """
-        writeDevice("/sys/kernel/debug/omap_mux/%s" % pinName , mode)
+        self.writeDevice("/sys/kernel/debug/omap_mux/%s" % pinName , mode)
     
     def __init__(self, channels, frequency):
         # Set up each channel
         for channel in channels:
-            pwm = pwm_map[results.channel]
+            pwm = Pwm.pwm_map[channel]
             dev = pwm['fs']
             
             # setup clock
-            setReg( pwm['clk'], 2 )
+            self.setReg( pwm['clk'], 2 )
 
-            if checkBusy(dev):
+            if self.checkBusy(dev):
                 raise Exception( "PWM channel %d (%s) is busy. You can try 'echo 0 > %s/request'" % (channel, dev, dev) )
 
             # set mux
-            setMuxMode(pwm['pin'], pwm['mode'])
+            self.setMuxMode(pwm['pin'], pwm['mode'])
 
             # set frequency etc
             duty_percent = self.getDutyPercent(dev)
@@ -190,14 +192,15 @@ class Pwm:
 class PwmDriver:
     def __init__( self ):
         self.channels = range(1,7)
-        self.frequency = rospy.get_param( "~pwm_frequency" )
-        self.period = 1/self.frequency * 1000000 # period in microseconds
+        self.frequency = rospy.get_param( "~pwm_frequency", 50 )
+        self.period = 1.0/self.frequency * 1000000 # period in microseconds
         self.pwm = Pwm( self.channels, self.frequency )
         self.pwm_topic = rospy.get_param( "~pwm_topic", "pwm" )
-        self.pwm_sub = rospy.Subscriber( self.pwm_topic, Pwm, self.pwm_cb )
+        self.pwm_sub = rospy.Subscriber( self.pwm_topic, falkor_msgs.msg.Pwm, self.pwm_cb )
 
     def pwm_cb(self, data):
         if len( data.pwm ) != len( self.channels ):
+	    import pdb; pdb.set_trace()	
             raise Exception( "Length of pwm data doesn't match the number of channels" )
 
         for i in zip( self.channels, data.pwm ):
