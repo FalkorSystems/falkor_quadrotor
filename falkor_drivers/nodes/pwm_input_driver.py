7374208253eb9eddda01dd7b9f2af64320619b78
Sameer Parekh Brenn <sameer@creativedestruction.com>
2012-11-19 10:46:12 -0800

    add a driver for pwm input

---
+++ falkor_drivers/nodes/pwm_input_driver.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest("falkor_drivers")
+import rospy
+from serial import *
+from falkor_msgs.msg import Pwm
+
+class PwmInputDriver:
+    def __init__(self, pin=0):
+        self.pwm_topic = rospy.get_param( "~input_topic", "pwm_input" )
+        self.pwm_pub = rospy.Publisher( self.pwm_topic, Pwm )
+        self.port_name = rospy.get_param( "~port", "/dev/ttyUSB0" )
+        self.baud = rospy.get_param( "~baud", 115200 )
+        self.timeout = rospy.get_param( "~timeout", 5.0 )
+
+        self.port = Serial(self.port_name, self.baud, timeout=self.timeout * 0.5)
+
+    def parse_line(self, line):
+        split = line.split(',')
+        split_ints = [int(a) for a in split]
+        return split_ints
+
+    def run(self):
+        while not rospy.is_shutdown():
+            line = self.port.readline()
+
+            try:
+                pwm_data = self.parse_line(line)
+            except Exception as e:
+                rospy.logwarn( "Invalid pwm data line: "  + str(e) )
+            else:
+                pwm_msg = Pwm( pwm_data )
+                self.pwm_pub.publish( pwm_msg )
+
+def main():
+    rospy.init_node('pwm_input_driver')
+    driver = PwmInputDriver()
+    try:
+        driver.run()
+    except KeyboardInterrupt:
+        print "Shutting down"
+
+if __name__  == '__main__':
+    main()
