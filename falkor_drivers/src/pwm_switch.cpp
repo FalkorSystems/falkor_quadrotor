d54a0ee160fda5ea7aedefbf578f1b2d13fab5bc
Sameer Parekh Brenn <sameer@creativedestruction.com>
2012-12-13 10:25:10 -0600

    switch to using shared ptrs

--- falkor_drivers/src/pwm_switch.cpp
+++ falkor_drivers/src/pwm_switch.cpp
@@ -1,15 +1,15 @@
 #include "ros/ros.h"
 #include "falkor_msgs/Pwm.h"
 
-falkor_msgs::Pwm pwm_in;
-falkor_msgs::Pwm pwm_cmd;
+falkor_msgs::Pwm::ConstPtr pwm_in;
+falkor_msgs::Pwm::ConstPtr pwm_cmd;
 
-void pwm_in_cb(const falkor_msgs::Pwm &in_msg)
+void pwm_in_cb(falkor_msgs::Pwm::ConstPtr in_msg)
 {
   pwm_in = in_msg;
 }
 
-void pwm_cmd_cb( const falkor_msgs::Pwm &in_msg)
+void pwm_cmd_cb( const falkor_msgs::Pwm::ConstPtr in_msg)
 {
   pwm_cmd = in_msg;
 }
@@ -19,11 +19,14 @@ int main(int argc, char **argv)
   ros::init( argc, argv, "pwm_switch" );
   ros::NodeHandle n;
 
+  falkor_msgs::Pwm pwm_default;
   for( int i = 0; i < 7; i++ )
     {
-      pwm_in.pwm.push_back( 1500 );
-      pwm_cmd.pwm.push_back( 1500 );
+      pwm_default.pwm.push_back( 1500 );
     }
+
+  pwm_in = falkor_msgs::Pwm::ConstPtr( new falkor_msgs::Pwm( pwm_default ) );
+  pwm_cmd = falkor_msgs::Pwm::ConstPtr( new falkor_msgs::Pwm( pwm_default ) );
 
   ros::Publisher pwm_out_pub = n.advertise<falkor_msgs::Pwm>("pwm_out", 1000);
   ros::Subscriber pwm_in_sub = n.subscribe( "pwm_in", 1000, pwm_in_cb );
@@ -34,19 +37,19 @@ int main(int argc, char **argv)
 
   while (ros::ok())
     {
-      falkor_msgs::Pwm pwm_out;
+      falkor_msgs::Pwm::Ptr pwm_out(new falkor_msgs::Pwm);
 
-      if(pwm_in.pwm.size() >= 5)
+      if(pwm_in->pwm.size() >= 5)
 	{
-	  if (pwm_in.pwm[4] > 1500)
+	  if (pwm_in->pwm[4] > 1500)
 	    {
-	      pwm_out = pwm_in;
-	      pwm_out.pwm[4] = chan_5_fix;
+	      pwm_out->pwm = pwm_in->pwm;
+	      pwm_out->pwm[4] = chan_5_fix;
 	    }
 	  else
 	    {
-	      pwm_out = pwm_cmd;
-	      pwm_out.pwm[4] = chan_5_fix;
+	      pwm_out->pwm = pwm_cmd->pwm;
+	      pwm_out->pwm[4] = chan_5_fix;
 	    }
 	  pwm_out_pub.publish( pwm_out );
 	}
