ce99146c02712cc4bf832cab03785bd567aae5ab
Sameer Parekh Brenn <sameer@creativedestruction.com>
2012-12-13 10:46:16 -0600

    turn into a class

--- falkor_drivers/src/pwm_switch.cpp
+++ falkor_drivers/src/pwm_switch.cpp
@@ -1,62 +1,76 @@
 #include "ros/ros.h"
 #include "falkor_msgs/Pwm.h"
 
-falkor_msgs::Pwm::ConstPtr pwm_in;
-falkor_msgs::Pwm::ConstPtr pwm_cmd;
+class PwmSwitch {
+private:
+  falkor_msgs::Pwm::ConstPtr pwm_in_;
+  falkor_msgs::Pwm::ConstPtr pwm_cmd_;
+  ros::NodeHandle nh_;
+  ros::NodeHandle nh_private_;
+  ros::Publisher pwm_out_pub_;
+  ros::Subscriber pwm_in_sub_;
+  ros::Subscriber pwm_cmd_sub_;
+  int chan_5_fix_;
+  ros::Timer timer_;
 
-void pwm_in_cb(falkor_msgs::Pwm::ConstPtr in_msg)
+public:
+  PwmSwitch(ros::NodeHandle nh, ros::NodeHandle nh_private):
+    nh_(nh),
+    nh_private_(nh_private)
+  {
+    falkor_msgs::Pwm pwm_default;
+    for( int i = 0; i < 7; i++ )
+      {
+	pwm_default.pwm.push_back( 1500 );
+      }
+
+    pwm_in_ = falkor_msgs::Pwm::ConstPtr( new falkor_msgs::Pwm( pwm_default ) );
+    pwm_cmd_ = falkor_msgs::Pwm::ConstPtr( new falkor_msgs::Pwm( pwm_default ) );
+
+    pwm_out_pub_ = nh_.advertise<falkor_msgs::Pwm>("pwm_out", 1000);
+    pwm_in_sub_ = nh_.subscribe( "pwm_in", 1000, &PwmSwitch::pwmInCallback, this );
+    pwm_cmd_sub_ = nh_.subscribe( "pwm_cmd", 1000, &PwmSwitch::pwmCmdCallback, this );
+    chan_5_fix_ = 1870;
+    timer_ = nh_.createTimer( ros::Duration(0.020), &PwmSwitch::loopOnce, this );
+  }
+
+  void pwmInCallback(falkor_msgs::Pwm::ConstPtr in_msg)
+  {
+    pwm_in_ = in_msg;
+  }
+
+  void pwmCmdCallback( const falkor_msgs::Pwm::ConstPtr in_msg)
+  {
+    pwm_cmd_ = in_msg;
+  }
+
+  void loopOnce(const ros::TimerEvent &event)
+  {
+    falkor_msgs::Pwm::Ptr pwm_out(new falkor_msgs::Pwm);
+
+    if(pwm_in_->pwm.size() >= 5)
+      {
+	if (pwm_in_->pwm[4] > 1500)
+	  {
+	    pwm_out->pwm = pwm_in_->pwm;
+	    pwm_out->pwm[4] = chan_5_fix_;
+	  }
+	else
+	  {
+	    pwm_out->pwm = pwm_cmd_->pwm;
+	    pwm_out->pwm[4] = chan_5_fix_;
+	  }
+	pwm_out_pub_.publish( pwm_out );
+      }
+  }
+};
+
+int main (int argc, char **argv)
 {
-  pwm_in = in_msg;
-}
-
-void pwm_cmd_cb( const falkor_msgs::Pwm::ConstPtr in_msg)
-{
-  pwm_cmd = in_msg;
-}
-
-int main(int argc, char **argv)
-{
-  ros::init( argc, argv, "pwm_switch" );
-  ros::NodeHandle n;
-
-  falkor_msgs::Pwm pwm_default;
-  for( int i = 0; i < 7; i++ )
-    {
-      pwm_default.pwm.push_back( 1500 );
-    }
-
-  pwm_in = falkor_msgs::Pwm::ConstPtr( new falkor_msgs::Pwm( pwm_default ) );
-  pwm_cmd = falkor_msgs::Pwm::ConstPtr( new falkor_msgs::Pwm( pwm_default ) );
-
-  ros::Publisher pwm_out_pub = n.advertise<falkor_msgs::Pwm>("pwm_out", 1000);
-  ros::Subscriber pwm_in_sub = n.subscribe( "pwm_in", 1000, pwm_in_cb );
-  ros::Subscriber pwm_cmd_sub = n.subscribe( "pwm_cmd", 1000, pwm_cmd_cb );
-  int chan_5_fix = 1870;
-
-  ros::Rate loop_rate(50);
-
-  while (ros::ok())
-    {
-      falkor_msgs::Pwm::Ptr pwm_out(new falkor_msgs::Pwm);
-
-      if(pwm_in->pwm.size() >= 5)
-	{
-	  if (pwm_in->pwm[4] > 1500)
-	    {
-	      pwm_out->pwm = pwm_in->pwm;
-	      pwm_out->pwm[4] = chan_5_fix;
-	    }
-	  else
-	    {
-	      pwm_out->pwm = pwm_cmd->pwm;
-	      pwm_out->pwm[4] = chan_5_fix;
-	    }
-	  pwm_out_pub.publish( pwm_out );
-	}
-
-      ros::spinOnce();
-      loop_rate.sleep();
-    }
-
+  ros::init (argc, argv, "PwmSwitch");
+  ros::NodeHandle nh;
+  ros::NodeHandle nh_private("~");
+  PwmSwitch pwm_switch(nh, nh_private);
+  ros::spin();
   return 0;
 }
