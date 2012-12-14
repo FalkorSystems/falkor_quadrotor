03b6a682ddee2c3fb10a4f407396f2ea0472d2e1
Sameer Parekh Brenn <sameer@creativedestruction.com>
2012-12-14 12:53:47 -0800

    add i2c driver

---
+++ falkor_drivers/nodes/i2c_driver.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest("falkor_drivers")
+import rospy
+from serial import *
+from sensor_msgs.msg import *
+from geometry_msgs.msg import *
+from std_msgs.msg import *
+from falkor_msgs.msg import *
+import numpy as np
+from i2c_sensors import *
+
+class RazorDriver:
+    def __init__(self):
+        self.timeout = rospy.get_param( "~timeout", 5.0 )
+        self.i2c_number = 3
+        self.i2c = I2CSensors( self.i2c_number )
+
+        self.mag_topic = rospy.get_param( "~mag_topic", "imu/mag" )
+        self.imu_topic = rospy.get_param( "~imu_topic", "imu/data_raw" )
+        self.baro_topic = rospy.get_param( "~baro_topic", "altimeter" )
+
+        self.tf_prefix = rospy.get_param( "~tf_prefix", "" )
+        self.mag_frame = self.tf_prefix + "/" + rospy.get_param( "~mag_frame", "magnetometer" )
+        self.imu_frame = self.tf_prefix + "/" + rospy.get_param( "~imu_frame", "imu" )
+        self.baro_frame = self.tf_prefix + "/" + rospy.get_param( "~baro_frame", "altimeter" )
+
+        self.mag_pub = rospy.Publisher( self.mag_topic, Vector3Stamped )
+        self.imu_pub = rospy.Publisher( self.imu_topic, Imu )
+        self.baro_pub = rospy.Publisher( self.baro_topic, Altimeter )
+
+        self.baro_timer = rospy.Timer( rospy.Duration( 1/rospy.get_param( "~baro_rate", 10 ) ),
+                                       self.baro_cb )
+        self.imu_timer = rospy.Timer( rospy.Duration( 1/rospy.get_param( "~imu_rate", 1000 ) ),
+                                      self.imu_cb )
+        self.mag_timer = rospy.Timer( rospy.Duration( 1/ropsy.get_param( "~mag_rate", 10 ) ),
+                                      self.mag_cb )
+
+    def self.baro_cb( self, event ):
+        baro_data = self.i2c.read_baro()
+        msg = Altimeter()
+        msg.header.frame_id = self.baro_frame
+        msg.altitude = baro_data[2]
+        msg.pressure = baro_data[1]
+        self.baro_pub.publish( msg )
+
+    def self.mag_cb( self, event ):
+        mag_data = self.i2c.read_mag()
+        mag_data = np.array( mag_data ) / np.linalg.norm( mag_data )
+        msg = Vector3Stamped()
+        msg.header.frame_id = self.mag_frame
+        msg.vector = Vector3( *mag_data )
+        # TODO: Rotate according to IMU orientation
+
+        self.mag_pub.publish( mag_msg )
+
+    def self.imu_cb( self, event ):
+        accel_data = self.i2c.read_accel()
+        gyro_data = self.i2c.gyro_data()
+        msg = Imu()
+
+        msg.header.frame_id = self.imu_frame
+        msg.orientation = Quaternion( 0, 0, 0, 1.0 )
+        msg.orientation_covariance = [0]*9
+
+        gyro_data = np.array( gyro_data ) / 14.375 / 180 * np.pi
+        msg.angular_velocity = Vector3( *gyro_data )
+        # TODO: Rotate according to IMU orientation        
+        
+        # We need to figure this out
+        msg.angular_velocity_covariance = [0] * 9
+
+        accel_data = np.array( accel_data ) / 256.0 * 9.82
+        msg.linear_acceleration = Vector3( *accel )
+        # TODO: Rotate according to IMU orientation        
+
+        # Figure this out too
+        msg.linear_acceleration_covariance = [0]*9
+
+        self.imu_pub.publish(msg)
+
+def main():
+    rospy.init_node('razor_driver')
+    driver = RazorDriver()
+    try:
+        driver.run()
+    except KeyboardInterrupt:
+        print "Shutting down"
+
+if __name__  == '__main__':
+    main()
+
