366b037de05da5dce5ef9edb1034ca2d4db98ba9
Sameer Parekh Brenn <sameer@creativedestruction.com>
2013-01-06 18:54:44 -0600

    fix i2c test program

--- falkor_drivers/src/i2c_test.cpp
+++ falkor_drivers/src/i2c_test.cpp
@@ -1,8 +1,13 @@
+#include <stdlib.h>
 #include <falkor_drivers/i2c.h>
     
 int main(void)
 {
   I2CDriver i2c(3);
+  std::vector<uint8_t> result = i2c.readI2CBlockData( 0x68, 0x1d, 32 );
 
-  i2c.writeByte( 0x70, 0x51 );
+  for( int i = 0; i < result.size(); i++ )
+    printf( "%X ", result[i] );
+
+  printf( "\n" );
 }
