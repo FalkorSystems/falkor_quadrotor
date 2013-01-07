fe6d02d5069cfea294ea33349f4b0907a6db451a
Sameer Parekh Brenn <sameer@creativedestruction.com>
2013-01-07 14:47:38 -0600

    full c++ i2c tester

--- falkor_drivers/src/i2c_test.cpp
+++ falkor_drivers/src/i2c_test.cpp
@@ -2,6 +2,7 @@
 #include <falkor_drivers/i2c.h>
 #include <falkor_drivers/sensor.h>
 #include <iostream>
+#include <unistd.h>
  
 void print_vector( const std::vector<double> &vec )
 {
@@ -17,6 +18,8 @@ void init_and_print( I2CSensor *i2c_sensor )
   try {
     i2c_sensor->init();
     print_vector( i2c_sensor->read() );
+
+    print_vector( i2c_sensor->read() );
   }
   catch( std::exception( e ) ) {
       std::cerr << e.what() << std::endl;
@@ -28,8 +31,10 @@ int main(void)
   I2CDriver i2c(3);
   Barometer barometer( 0x77, &i2c);
   Accelerometer accelerometer( 0x53, &i2c );
-  Magnetometer magnetometer( 0x1E, &i2c, 0.0, 0.0, 0.0, 0.0, 0, 466, 433, 453 );
+  Magnetometer magnetometer( 0x1E, &i2c, 0.0, 0.0, 1.0, 1.0, 0.0, 466, 433, 453 );
   Gyrometer gyrometer( 0x68, &i2c );
+
+  i2c.writeByteData( 0x1E, 0x01, 0x70 );
   
   std::cout << "baro" << std::endl;
   init_and_print( &barometer );
